0549fc5079f19a4a250bae492c95f468e9078daf
jedr <andrzej.stencel@gmail.com>
2020-09-16 14:53:07 +0200

    Initial commit

---
+++ SpanNameInHttpClientInstrumentation/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Trace;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SpanNameInHttpClientInstrumentation
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var tracerProvider = Sdk
+                .CreateTracerProviderBuilder()
+                .AddHttpClientInstrumentation()
+                .AddConsoleExporter()
+                .Build();
+
+            using var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync("http://api.icndb.com/jokes/random");
+        }
+    }
+}
